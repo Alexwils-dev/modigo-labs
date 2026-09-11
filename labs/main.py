d760653ba3072d3c7e7bdf0f6582fb1e6931cc7d
Alexwils-dev <tekswilson@gmail.com>
2026-09-11 12:54:53 +0100

    Complete: Below is a function meant to generate a greeting card messag

--- labs/main.py
+++ labs/main.py
@@ -1,4 +1,4 @@
-def apply_discount(price, discount_percent=10):
-    return round(price - (price * discount_percent / 100), 2)
+def create_greeting(recipient_name, sender_name="A Friend", closing="Best wishes"):
+    return (f"Dear {recipient_name}, {closing}! From, {sender_name}.")
 
-print(apply_discount(100))
+print(create_greeting("Ada"))
