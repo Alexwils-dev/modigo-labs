1c0c46fc72e560f131cf7201121512db641da8dd
Alexwils-dev <tekswilson@gmail.com>
2026-09-11 11:06:09 +0100

    Complete: Vowel Set Checker

---
+++ challenges/main.py
@@ -0,0 +1,9 @@
+def has_all_vowels(word):
+    required = {"a", "e", "i", "o", "u"}
+    word = word.lower()
+    # TODO: build a set of vowels actually found in `word`,
+    # then check if it contains all of `required`
+    found = {ch for ch in word if ch in required}
+    
+    return required.issubset(found)
+print(has_all_vowels("education"))
