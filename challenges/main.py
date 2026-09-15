c3de996d0b5a3a8e5568f53d00a2f7b5ef446bd0
Alexwils-dev <tekswilson@gmail.com>
2026-09-15 12:40:38 +0100

    Complete: Favorite Color Survey

--- challenges/main.py
+++ challenges/main.py
@@ -1,9 +1,22 @@
-def total_word_count(sentences):
-    total = 0
-    for sentence in sentences:
-        words = sentence.split()
-        count = len(words)
-        total += count
-    return total
+def analyze_survey(responses):
+    if not responses:
+        return {"tally": {}, "most_popular": None}
 
-print(total_word_count(["hello world", "how are you"]))
+    tally = {}
+
+    for color in responses:
+        if color not in tally:
+            tally[color] = 0
+        tally[color] += 1
+            
+
+    most_popular = None
+    highest_count = 0
+    
+    for color, count in tally.items():
+        if count > highest_count:
+            highest_count = count
+            most_popular = color
+
+    return {"tally": tally, "most_popular": most_popular}
+print(analyze_survey(["red", "blue", "red", "green"]))
