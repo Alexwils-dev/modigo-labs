4f83a0c93dcbe44194599851538776244bf611c8
Alexwils-dev <tekswilson@gmail.com>
2026-09-15 11:33:27 +0100

    Complete: Fix the Word Counter

--- challenges/main.py
+++ challenges/main.py
@@ -1,11 +1,9 @@
-def count_unique_coordinates(coordinates):
-    if not coordinates:
-        return 0
+def total_word_count(sentences):
+    total = 0
+    for sentence in sentences:
+        words = sentence.split()
+        count = len(words)
+        total += count
+    return total
 
-    unique = []
-    for coord in coordinates:
-        if coord not in unique:
-            unique.append(coord)
-    return len(unique)
-
-print(count_unique_coordinates([(0,0), (1,1), (0,0)]))
+print(total_word_count(["hello world", "how are you"]))
