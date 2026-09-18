9b5edbab24c14b02503cf52f4bb9536134c25ba4
Alexwils-dev <tekswilson@gmail.com>
2026-09-18 10:55:51 +0100

    Complete: Roman Numeral Converter

--- challenges/main.py
+++ challenges/main.py
@@ -1,8 +1,12 @@
-def double_values(numbers):
-    doubled = []
+def to_roman(number):
+    values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    result = ""
 
-    for number in numbers:
-        doubled.append(number * 2)
+    for value, symbol in zip(values, symbols):
+        count = number // value
+        result += symbol * count
+        number = number % value
 
-    return doubled
-print(double_values([1, 2, 3]))
+    return result
+print(to_roman(14))
