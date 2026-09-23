7530f689092b01c86998f6afb956539872ca237f
Alexwils-dev <tekswilson@gmail.com>
2026-09-23 14:51:04 +0100

    Complete: Market Day Sales Tracker

--- challenges/main.py
+++ challenges/main.py
@@ -1,8 +1,24 @@
-def find_max(numbers):
-    largest = numbers[0]
+def analyze_sales(prices):
+    total = 0
+    highest = prices[0]
+    lowest = prices[0]
 
-    for num in numbers:
-        if num > largest:
-            largest = num
-    return largest
-print(find_max([3, 7, 2]))
+    for price in prices:
+        total += price
+
+        if price > highest:
+            highest = price
+
+        if price < lowest:
+            lowest = price
+
+    average = round(total / len(prices), 2)
+
+    return {
+        "total": total,
+        "average": average,
+        "highest": highest,
+        "lowest": lowest
+    }
+
+print(analyze_sales([500, 1200, 300]))
